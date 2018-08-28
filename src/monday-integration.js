f5a6412ad3b6f7af022256bf7940e1d3cea4acb7
Sergey Bolshchikov <sergey@bolshchikov.net>
2018-08-28 16:50:54 +0300

    fix form part

--- src/monday-integration.js
+++ src/monday-integration.js
@@ -5,12 +5,14 @@ const USER_ID = process.env.USER_ID;
 const MONDAY_API_TOKEN = process.env.MONDAY_API_TOKEN;
 
 const url = `https://api.monday.com:443/v1/boards/${NEW_HIRE_BOARD_ID}/pulses.json?api_key=${MONDAY_API_TOKEN}`;
-console.log(url);
 
 const addNewHire = (name) => {
   return axios.post(
     url,
-    `user_id=100637&pulse%5Bname%5D=${name}`,
+    {
+      "user_id": USER_ID,
+      "pulse[name]": name,
+    },
     { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
   )
 };
