100586fcfc64634cda1b40e5cbd83e17eaf22b8b
Sergey Bolshchikov <sergey@bolshchikov.net>
2018-08-28 16:38:16 +0300

    fix monday integration

--- src/monday-integration.js
+++ src/monday-integration.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const NEW_HIRE_BOARD_ID = process.env.NEW_HIRE_BOARD_ID;
 const USER_ID = process.env.USER_ID;
-const MONDAY_API_TOKEN = process.env.API_TOKEN;
+const MONDAY_API_TOKEN = process.env.MONDAY_API_TOKEN;
 
 const url = `https://api.monday.com:443/v1/boards/${NEW_HIRE_BOARD_ID}/pulses.json?api_key=${MONDAY_API_TOKEN}`;
 console.log(url);
@@ -11,7 +11,7 @@ const addNewHire = (name) => {
   return axios.post(url, {
     "user_id": USER_ID,
     "pulse[name]": name
-  })
+  }, { headers: { 'Content-Type': 'multipart/form-data' } })
 };
 
 module.exports = {
