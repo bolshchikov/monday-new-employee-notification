ca6d52c5c7ab4df01b7aa64e95b827620a8ff109
Sergey Bolshchikov <sergey@bolshchikov.net>
2018-08-28 12:25:23 +0300

    read body message

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
 const PORT = process.env.PORT || 1337;
 
-app.post('/parse', (req, res) => res.send(200));
+app.use(bodyParser.json());
+
+app.post('/parse', (req, res) => {
+  res.send(req.body);
+});
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => console.log('Example app listening on port 3000!'));
+app.listen(PORT, () => console.log(`The app is running at ${PORT}`));
