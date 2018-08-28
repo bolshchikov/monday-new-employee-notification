92f650e33515b7efa75bd1e549073ff976bef2df
Sergey Bolshchikov <sergey@bolshchikov.net>
2018-08-28 17:52:19 +0300

    use node fetch

--- src/monday-integration.js
+++ src/monday-integration.js
@@ -1,4 +1,4 @@
-const FormData = require('form-data');
+const fetch = require('node-fetch');
 
 const NEW_HIRE_BOARD_ID = process.env.NEW_HIRE_BOARD_ID;
 const USER_ID = process.env.USER_ID;
@@ -7,16 +7,15 @@ const MONDAY_API_TOKEN = process.env.MONDAY_API_TOKEN;
 const url = `https://api.monday.com:443/v1/boards/${NEW_HIRE_BOARD_ID}/pulses.json?api_key=${MONDAY_API_TOKEN}`;
 
 const addNewHire = (name) => {
-  const form = new FormData();
-  form.append("user_id", USER_ID);
-  form.append("pulse[name]", name);
-  
-  return new Promise((resolve, reject) => {
-    form.submit(url, (err, res) => {
-      if (err) reject(err);
-      resolve(res);
-    })
-  });
+  fetch(url, {
+    "credentials": "omit",
+    "headers": { "content-type": "application/x-www-form-urlencoded" },
+    "referrerPolicy": "no-referrer-when-downgrade",
+    "body": `user_id=${USER_ID}&pulse%5Bname%5D=${name}`,
+    "method": "POST",
+    "mode": "cors"
+  }).then(res => res.json());
+
 };
 
 module.exports = {
