fddc58e8c782a09b78c4cbaa009bd6dc02b1edf4
Sergey Bolshchikov <sergey@bolshchikov.net>
2018-08-29 15:20:47 +0300

    parse body

--- src/message-parser.js
+++ src/message-parser.js
@@ -1,14 +1,42 @@
-const extractName = (subject) => {
+const cheerio = require('cheerio');
 
+const extractName = (subject) => {
   const space = 1;
   const keyword = 'Hire';
   const firstIndex = subject.indexOf(keyword);
   const secondIndex = subject.indexOf('(');
 
   return subject.substring(firstIndex + keyword.length + space, secondIndex - space);
-
 };
 
+const normalizeDate = (raw) => {
+  const date = new Date(raw);
+  const year = date.getFullYear();
+  const month = date.getMonth() < 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+  return `${year}-${month}-${day}`;
+}
+
+const extractBodyValues = (bodyHTMLString) => {
+  const TEAM_INDEX = 3;
+  const TEAM_LEAD_INDEX = 5;
+  const JOIN_DATE_INDEX = 6;
+
+  const $ = cheerio.load(bodyHTMLString);
+  const allImportantTags = $('b');
+
+  const teamName = $(allImportantTags[TEAM_INDEX]).text();
+  const teamLeadName = $(allImportantTags[TEAM_LEAD_INDEX]).text();
+  const joinDate = normalizeDate($(allImportantTags[JOIN_DATE_INDEX]).text());
+
+  return {
+    teamName,
+    teamLeadName,
+    joinDate
+  };
+}
+
 module.exports = {
-  extractName
+  extractName,
+  extractBodyValues
 };
--- test/message-parser.spec.js
+++ test/message-parser.spec.js
@@ -1,10 +1,23 @@
-const { extractName } = require('../src/message-parser');
 const { expect } = require('chai');
+const messageBody = require('./fixtures/message-body');
+const { extractName, extractBodyValues } = require('../src/message-parser');
 
 describe('subject parser', () => {
   it('should extract name', () => {
     const testName = 'Vasya Anchlovich';
     const testSubject = `HR Notification - New Hire ${testName} (123456)`;
     expect(extractName(testSubject)).to.equal(testName);
-  })
+  });
+
+  it('should extract values from body', () => {
+    const {
+      teamName,
+      teamLeadName,
+      joinDate
+    } = extractBodyValues(messageBody);
+
+    expect(teamName).to.equal('Promote');
+    expect(teamLeadName).to.equal('Guy Kori');
+    expect(joinDate).to.equal('2018-10-02');
+  });
 });
